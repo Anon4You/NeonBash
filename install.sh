1e8de475cbad2d7e4b6a16c8160d945a93b1c0a0
Alienkrishn <60428430+Anon4You@users.noreply.github.com>
2026-05-09 18:38:44 +0530

    Update URL for termuxvoid installation script

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ case "$answer" in n|N) echo "Exiting."; rm -- "$0"; exit 0;; *) echo "Starting i
 rm -rf $PREFIX/etc/motd
 apt install -y ncurses-utils
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/termuxvoid.list" ]; then
-    bash <(curl -sL is.gd/termuxvoid) -s
+    bash <(curl -sL https://is.gd/termuxvoid) -s
 fi
 
 if ! command -v apt &>/dev/null; then
